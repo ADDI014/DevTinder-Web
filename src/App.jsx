d875cff324b34025b0b634a6a134ce20290a5a25
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-21 21:25:29 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import Navbar from "./components/Navbar"
+import Body from "./components/Body";
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
     return (
     <>
-      <Navbar/>
+
+      <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-          <div className="navbar bg-base-100 shadow-sm">
+          <div className="navbar bg-base-200 shadow-sm">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">👨‍💻devTinder</a>
   </div>
