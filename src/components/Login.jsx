581fd3cf29f385d9a667b48099f3dcfaa476ec68
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-21 21:40:49 +0530

    login page UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const Login = () => {
   return (
+    <div className='flex justify-center my-10'>
+      <div className="card card-border bg-base-300 w-96">
+  <div className="card-body">
+    <h2 className="card-title flex justify-center">Login</h2>
     <div>
-      Login
+      <fieldset className="fieldset">
+  <legend className="fieldset-legend">Email</legend>
+  <input type="text" className="input" />
+</fieldset>
+
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend">Password</legend>
+  <input type="text" className="input" />
+</fieldset>
+    </div>
+   
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary">Login</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
