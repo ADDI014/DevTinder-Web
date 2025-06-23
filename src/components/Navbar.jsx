353c21bb4cd9935ca10b5f4aeca71c33f180f6f7
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-23 16:18:41 +0530

    set cookies so if we refresh then it should still login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
@@ -8,7 +9,7 @@ const Navbar = () => {
     <div>
       <div className="navbar bg-base-200 shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">👨‍💻devTinder</a>
+          <Link to={"/"} className="btn btn-ghost text-xl">👨‍💻devTinder</Link>
         </div>
 
         <div className="flex items-center gap-4 mr-5">
@@ -26,7 +27,7 @@ const Navbar = () => {
                   tabIndex={0}
                   className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
                 >
-                  <li><a className="justify-between">Profile</a></li>
+                  <li><Link to={"/profile"} className="justify-between">Profile</Link></li>
                   <li><a>Settings</a></li>
                   <li><a>Logout</a></li>
                 </ul>
