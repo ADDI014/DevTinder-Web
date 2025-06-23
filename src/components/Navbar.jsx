7b8fe82cbca2aa961e19a072835a54b4c196e48f
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-23 17:44:13 +0530

    building logout feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
+import axios from 'axios';
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom';
+import { BASR_URL } from '../utils/constants';
+import { removeUser } from '../utils/userSlice';
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASR_URL + "/logout" , {} , {
+        withCredentials : true,
+      })
+
+      dispatch(removeUser());
+      return navigate("/login");
+    }
+    catch(err){
+      //Error Logic
+    }
+  }
+
 
   return (
     <div>
@@ -29,7 +49,7 @@ const Navbar = () => {
                 >
                   <li><Link to={"/profile"} className="justify-between">Profile</Link></li>
                   <li><a>Settings</a></li>
-                  <li><a>Logout</a></li>
+                  <li><a onClick={handleLogout}>Logout</a></li>
                 </ul>
               </div>
             </>
