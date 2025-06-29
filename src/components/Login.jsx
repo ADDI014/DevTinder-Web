d0b3abb1aea936b943b737f6ccb3fc28762a307c
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-06-29 18:31:57 +0530

    built signUp UI in existing signin compopent using condition and states

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,6 +11,10 @@ const Login = () => {
 const [emailId , setEmailId] = useState("");
 const [password, setPassword] = useState("");
 const [error, setError] = useState("");
+const [firstName , setFirstName] = useState("");
+const [lastName, setLastName] = useState("");
+const [isLogin, setIsLogin] = useState(true);
+
 
 const dispatch = useDispatch();
 const navigate = useNavigate();
@@ -38,24 +42,38 @@ const handleLogin = async () => {
     <div className='flex justify-center my-10'>
       <div className="card card-border bg-base-300 w-96">
   <div className="card-body">
-    <h2 className="card-title flex justify-center">Login</h2>
+    <h2 className="card-title flex justify-center">{isLogin ? "Login" : "Sign Up"}</h2>
+      {!isLogin &&<div>
+      <fieldset className="fieldset">
+  <legend className="fieldset-legend">FirstName</legend>
+  <input type="text" className="input" value={firstName}  onChange={(e) => setFirstName(e.target.value) } />
+</fieldset>
+
+    <fieldset className="fieldset">
+  <legend className="fieldset-legend">LastName</legend>
+  <input type="password" className="input" value={lastName} onChange={(e) => setLastName(e.target.value) } />
+</fieldset>
+    </div>}
     <div>
       <fieldset className="fieldset">
   <legend className="fieldset-legend">Email</legend>
-  <input type="text" className="input"  onChange={(e) => setEmailId(e.target.value) } />
+  <input type="text" className="input" value={emailId} onChange={(e) => setEmailId(e.target.value) } />
 </fieldset>
 
     <fieldset className="fieldset">
   <legend className="fieldset-legend">Password</legend>
-  <input type="password" className="input"  onChange={(e) => setPassword(e.target.value) } />
+  <input type="password" className="input"  value={password} onChange={(e) => setPassword(e.target.value) } />
 </fieldset>
     </div>
 
     <p className='text-red-700'>{error}</p>
    
     <div className="card-actions justify-center">
-      <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+      <button className="btn btn-primary" onClick={handleLogin}>{isLogin ? "Login" : "Sign Up"}</button>
     </div>
+
+    <p className='m-auto my-5 cursor-pointer' onClick={() => setIsLogin((value) => !value)}>{isLogin ? "New User ? register Here" : "Existing User ? Login Here"}</p>
+
   </div>
 </div>
     </div>
