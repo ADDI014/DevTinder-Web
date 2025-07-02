9dfa17c5a540c8594e123da486e7e2922a6ad368
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-07-02 21:58:09 +0530

    some changes

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 
-export const BASR_URL = "https://devsang-backend.onrender.com/"
+export const BASR_URL = "https://devsang-backend.onrender.com"
 
 // export const BASR_URL = import.meta.env.BACKEND_URL;
 
