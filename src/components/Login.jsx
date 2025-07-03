5f51b7fd57231cf518bdf7ecaa687270cc453030
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-07-03 12:49:40 +0530

    imporove login & signUp UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -53,40 +53,40 @@ const handleSignUp = async () => {
 }
 
   return (
-    <div className='flex justify-center my-30'>
-      <div className="card card-border bg-base-300 w-96">
+    <div className='flex justify-center items-center min-h-screen bg-cover bg-center' style={{backgroundImage : "url('https://images.unsplash.com/photo-1503264116251-35a269479413?auto=format&fit=crop&w=1350&q=80')"}}>
+      <div className="card bg-base-100 bg-opacity-90 shadow-2xl w-full max-w-md p-4 rounded-2xl">
   <div className="card-body">
-    <h2 className="card-title flex justify-center">{isLogin ? "Login" : "Sign Up"}</h2>
+    <h2 className="card-title flex justify-center text-2xl font-bold mb-4 text-purple-700">{isLogin ? "Login to DevSang 💻❤️" : "Sign Up for DevSang 💻🔥"}</h2>
       {!isLogin &&<div>
-      <fieldset className="fieldset">
-  <legend className="fieldset-legend">FirstName</legend>
-  <input type="text" className="input" value={firstName}  onChange={(e) => setFirstName(e.target.value) } />
+      <fieldset className="mb-3">
+  <legend className="text-sm font-semibold text-gray-500">FirstName</legend>
+  <input type="text" className="input input-bordered w-full my-1" value={firstName}  onChange={(e) => setFirstName(e.target.value) } />
 </fieldset>
 
-    <fieldset className="fieldset">
-  <legend className="fieldset-legend">LastName</legend>
-  <input type="text" className="input" value={lastName} onChange={(e) => setLastName(e.target.value) } />
+    <fieldset className="mb-3">
+  <legend className="text-sm font-semibold text-gray-500">LastName</legend>
+  <input type="text" className="input input-bordered w-full my-1" value={lastName} onChange={(e) => setLastName(e.target.value) } />
 </fieldset>
     </div>}
     <div>
-      <fieldset className="fieldset">
-  <legend className="fieldset-legend">Email</legend>
-  <input type="text" className="input" value={emailId} onChange={(e) => setEmailId(e.target.value) } />
+      <fieldset className="mb-3">
+  <legend className="text-sm font-semibold text-gray-500">Email</legend>
+  <input type="text" className="input input-bordered w-full my-1" value={emailId} onChange={(e) => setEmailId(e.target.value) } />
 </fieldset>
 
-    <fieldset className="fieldset">
-  <legend className="fieldset-legend">Password</legend>
-  <input type="password" className="input"  value={password} onChange={(e) => setPassword(e.target.value) } />
+    <fieldset className="mb-3">
+  <legend className="text-sm font-semibold text-gray-500">Password</legend>
+  <input type="password" className="input input-bordered w-full my-1"  value={password} onChange={(e) => setPassword(e.target.value) } />
 </fieldset>
     </div>
 
-    <p className='text-red-700'>{error}</p>
+    {error && <p className='text-red-600 text-center mb-2'>{error}</p>}
    
     <div className="card-actions justify-center">
-      <button className="btn btn-primary" onClick={isLogin ?  handleLogin : handleSignUp}>{isLogin ? "Login" : "Sign Up"}</button>
+      <button className="btn btn-primary w-full" onClick={isLogin ?  handleLogin : handleSignUp}>{isLogin ? "Login" : "Sign Up"}</button>
     </div>
 
-    <p className='m-auto my-5 cursor-pointer' onClick={() => setIsLogin((value) => !value)}>{isLogin ? "New User ? register Here" : "Existing User ? Login Here"}</p>
+    <p className='text-center mt-4 text-blue-600 underline cursor-pointer' onClick={() => setIsLogin((value) => !value)}>{isLogin ? "New User? Register Here" : "Existing User? Login Here"}</p>
 
   </div>
 </div>
